2a2e36ef9087d349f77b5fc039ca544ea5dc03f3
Aleksandr Dobrolubov <dobrolubovforhtc@gmail.com>
2022-12-08 17:49:40 +0300

    changes format

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 public class Main {
     public static void main(String[] args) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        System.out.println(formatter.format(new Date()));
 
-        System.out.println(new Date());
     }
 }
